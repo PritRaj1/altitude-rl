b085612bd8c1b335a527b697f368be28d95b86a6
PritRaj1 <prithvi.raj1@outlook.com>
2026-06-08 18:30:37 -0700

    dev env

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Mars Lander / C++ Concurrency";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          gcc
+          gnumake
+        ];
+
+        shellHook = ''
+        echo "Entered dev environment"
+        '';
+      };
+    };
+}
