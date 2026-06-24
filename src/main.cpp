886587d488873d71cb22af16aac0973535c71e70
PritRaj1 <prithvi.raj1@outlook.com>
2026-06-24 13:59:37 -0700

    nvm still not there

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 int main() {
   MarsLanderEnv env;
-  TDtype td_type = TDtype::SARSA;
+  TDtype td_type = TDtype::QLearning;
 
   Agent global_agent(0.001, 1.0, 1.0, env);
   ThreadSafeReplayBuffer replay_buffer;
@@ -49,7 +49,7 @@ int main() {
     int action = global_agent.choose_action(s, true); // true = pure greedy
     return global_agent.get_thrust(action);
   };
-  log2csv(rl_controller, "sarsa.csv");
+  log2csv(rl_controller, "q_learning.csv");
 
   cout << "Logged to q_learning.csv, starting PID.";
 
